3cc1ece8ee7ebfc6d83dbe5825d7a907c145adcb
JHerminio <kulitpo25@gmail.com>
2022-03-02 11:05:22 +0800

    commit

--- home.js
+++ home.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["YouTuber", "Developer", "Blogger","Designer"],
-        typeSpeed: 200,
+        typeSpeed: 60,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer"],
-        typeSpeed: 200,
+        typeSpeed: 60,
         backSpeed: 60,
         loop: true
     });
